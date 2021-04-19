1917dbc214ba75bf7484a20575e1936cd09b9dad
Michał Skoczek <mwskoczek@gmail.com>
2021-04-19 15:59:43 +0200

    Created event-list-edit component, skip tests

---
+++ src/app/events/event-list/event-list-edit/event-list-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-list-edit',
+  templateUrl: './event-list-edit.component.html',
+  styleUrls: ['./event-list-edit.component.css']
+})
+export class EventListEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
