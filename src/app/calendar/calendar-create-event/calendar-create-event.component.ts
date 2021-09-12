e2141b7cd996df2d144455b4f2a07a965135f39d
Michał Skoczek <mwskoczek@gmail.com>
2021-09-12 18:21:57 +0200

    Created new component calendar-create-event

---
+++ src/app/calendar/calendar-create-event/calendar-create-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-create-event',
+  templateUrl: './calendar-create-event.component.html',
+  styleUrls: ['./calendar-create-event.component.css']
+})
+export class CalendarCreateEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
