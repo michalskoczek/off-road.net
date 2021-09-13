22f185414aae15843f7503a7a9474bc68ff5e550
Michał Skoczek <mwskoczek@gmail.com>
2021-09-13 17:45:52 +0200

    Added event types Event

--- src/app/calendar/calendar-list/calendar-list.component.ts
+++ src/app/calendar/calendar-list/calendar-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Event } from 'src/app/shared/event.model';
 
 @Component({
   selector: 'app-calendar-list',
   templateUrl: './calendar-list.component.html',
-  styleUrls: ['./calendar-list.component.css']
+  styleUrls: ['./calendar-list.component.css'],
 })
 export class CalendarListComponent implements OnInit {
+  @Input() event: Event;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
