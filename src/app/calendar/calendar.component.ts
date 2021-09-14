0bf85d8d9e63bfc5b1031c2ed05cfd6e25905f77
Michał Skoczek <mwskoczek@gmail.com>
2021-09-14 18:23:10 +0200

    Removed events array and added router-outlet in html

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,25 +7,6 @@ import { Event } from '../shared/event.model';
   styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent implements OnInit {
-  events: Event[] = [
-    new Event(
-      'Bałtowskie Bezdroża',
-      'Świętokrzyskie',
-      'Bałtów Dragon Which',
-      'rajd',
-      '07.09',
-      'SB'
-    ),
-    new Event(
-      'II rajd SUV-ów',
-      'Mazowieckie',
-      'Terenwizja',
-      'rajd',
-      '15.09',
-      'NDZ'
-    ),
-    new Event('Wyrowisko', 'Łódzkie', 'Wyrowiskowy', 'wyprawa', '29.11', 'NDZ'),
-  ];
   constructor() {}
 
   ngOnInit(): void {}
