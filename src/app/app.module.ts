ec055c54852e40ee6081bee63c1adad30d5e41de
Michał Skoczek <mwskoczek@gmail.com>
2021-09-14 16:31:20 +0200

    Added HomepageComponent to empty path and to app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CalendarCreateEventComponent } from './calendar/calendar-create-event/calendar-create-event.component';
 import { CalendarListComponent } from './calendar/calendar-list/calendar-list.component';
 import { CalendarEventComponent } from './calendar/calendar-event/calendar-event.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { CalendarEventComponent } from './calendar/calendar-event/calendar-event
     CalendarCreateEventComponent,
     CalendarListComponent,
     CalendarEventComponent,
+    HomepageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
