0a454c7303e4a53baf3e1e4c2fe6f871a16dd16a
Michał Skoczek <mwskoczek@gmail.com>
2021-09-14 18:20:12 +0200

    Added two paths as children create-event, calendar-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarCreateEventComponent } from './calendar/calendar-create-event/calendar-create-event.component';
 import { CalendarEventComponent } from './calendar/calendar-event/calendar-event.component';
+import { CalendarListComponent } from './calendar/calendar-list/calendar-list.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
@@ -15,8 +17,16 @@ const appRoutes: Routes = [
     component: CalendarComponent,
     children: [
       {
+        path: 'calendar-list',
+        component: CalendarListComponent,
+      },
+      {
         path: 'event',
         component: CalendarEventComponent,
+      },
+      {
+        path: 'create-event',
+        component: CalendarCreateEventComponent,
       },
     ],
   },
