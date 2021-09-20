f4dc9551a21ec7abe894157b16ce6de487097607
Michał Skoczek <mwskoczek@gmail.com>
2021-09-20 10:17:20 +0200

    Added buttons to service and used method getButtons to return array of buttons

--- src/app/options-panel/options.service.ts
+++ src/app/options-panel/options.service.ts
@@ -1,7 +1,17 @@
 export class OptionsPanelService {
   private options: string[] = ['rajdy', 'wyprawy', 'turystyka', 'wyścigi'];
+  private buttons: { routeName: string; buttonsName: string[] }[] = [
+    {
+      routeName: 'calendar',
+      buttonsName: ['Nowe wydarzenie', 'Lista wydarzeń'],
+    },
+  ];
 
   getOptions() {
     return this.options.slice();
+  }
+
+  getButtons() {
+    return this.buttons.slice();
   }
 }
