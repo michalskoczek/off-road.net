9629ed127c8677eac23619a9c1b7e80adf1e5dc4
Michał Skoczek <mwskoczek@gmail.com>
2021-09-21 14:38:30 +0200

    Display calendar-event by id param in url

--- src/app/calendar/calendar-event/calendar-event.component.ts
+++ src/app/calendar/calendar-event/calendar-event.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Event } from 'src/app/shared/event.model';
+import { EventService } from '../event.service';
 
 @Component({
   selector: 'app-calendar-event',
   templateUrl: './calendar-event.component.html',
-  styleUrls: ['./calendar-event.component.css']
+  styleUrls: ['./calendar-event.component.css'],
 })
 export class CalendarEventComponent implements OnInit {
+  event: Event;
+  id: number;
 
-  constructor() { }
+  constructor(
+    private eventService: EventService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.event = this.eventService.getEvent(this.id);
+    });
   }
-
 }
