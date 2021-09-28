d4d6920f949493741d771031fd8acbc7c79375a9
Michał Skoczek <mwskoczek@gmail.com>
2021-09-28 20:57:57 +0200

    Removed component calendar-create-event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { StoresComponent } from './stores/stores.component';
 import { BlogsComponent } from './blogs/blogs.component';
 import { OptionsPanelComponent } from './options-panel/options-panel.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CalendarCreateEventComponent } from './calendar/calendar-create-event/calendar-create-event.component';
 import { CalendarListComponent } from './calendar/calendar-list/calendar-list.component';
 import { CalendarEventComponent } from './calendar/calendar-event/calendar-event.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -28,7 +27,6 @@ import { CalendarEventDetailComponent } from './calendar/calendar-event/calendar
     StoresComponent,
     BlogsComponent,
     OptionsPanelComponent,
-    CalendarCreateEventComponent,
     CalendarListComponent,
     CalendarEventComponent,
     HomepageComponent,
