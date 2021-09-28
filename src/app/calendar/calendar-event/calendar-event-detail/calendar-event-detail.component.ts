4f6fd76bc26e6bfadefe36f45524c90fbc035a34
Michał Skoczek <mwskoczek@gmail.com>
2021-09-28 21:04:47 +0200

    Added calendar-event-detail to show event details

---
+++ src/app/calendar/calendar-event/calendar-event-detail/calendar-event-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { EventService } from '../../event.service';
+import { Event } from '../../../shared/event.model';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-calendar-event-detail',
+  templateUrl: './calendar-event-detail.component.html',
+  styleUrls: ['./calendar-event-detail.component.css'],
+})
+export class CalendarEventDetailComponent implements OnInit, OnDestroy {
+  event: Event;
+  id: number;
+  subscription: Subscription;
+
+  constructor(
+    private eventService: EventService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription = this.activatedRoute.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.event = this.eventService.getEvent(this.id);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
