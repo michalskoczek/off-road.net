d786ea780e114278a2a03f71f1b6fcc0b51c45b8
Michał Skoczek <mwskoczek@gmail.com>
2021-10-20 23:11:30 +0200

    Created function onFilterEvents

--- src/app/options-panel/options-panel.component.ts
+++ src/app/options-panel/options-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { FilterPipe } from '../filter.pipe';
 
 import { OptionsPanelService } from './options.service';
 
@@ -12,6 +14,7 @@ import { OptionsPanelService } from './options.service';
 export class OptionsPanelComponent implements OnInit {
   options: string[];
   buttons: string[];
+  filterPipe = new Subject<string>();
 
   constructor(
     private optionsService: OptionsPanelService,
@@ -22,5 +25,26 @@ export class OptionsPanelComponent implements OnInit {
   ngOnInit(): void {
     this.buttons = this.optionsService.getButtonsCalendar();
     this.options = this.optionsService.getOptionsCalendar();
+  }
+
+  // onFilterEvents(buttonName) {
+  //   console.log(buttonName.target.innerText);
+  //   switch (buttonName.target.innerText) {
+  //     case 'rajdy':
+  //       break;
+  //     case 'wyprawy':
+  //       break;
+  //     case 'turystyka':
+  //       break;
+  //     case 'wyścigi':
+  //       break;
+
+  //     default:
+  //       break;
+  //   }
+  // }
+
+  onFilterEvents(buttonName) {
+    this.filterPipe.next(buttonName.target.innerText);
   }
 }
