cd0a5ad7c3b501982bbedff03889f6dba7917af6
Michał Skoczek <mwskoczek@gmail.com>
2022-01-10 16:40:12 +0100

    Updated submit method - login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,34 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  isLoading: boolean = false;
+  error: string = null;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
     console.log(form);
+
+    if (!form.valid) return;
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+
+    this.authService.signin(email, password).subscribe(
+      (res) => {
+        console.log(res);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    form.reset();
   }
 }
