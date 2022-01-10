a9747bc979d206677996adfc17ba377c2276e2dd
Michał Skoczek <mwskoczek@gmail.com>
2022-01-10 16:38:07 +0100

    Created handleError function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,10 +23,53 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string): Observable<AuthResponseData> {
-    return this.http.post<AuthResponseData>(this.signupApiUrl, {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    });
+    return this.http
+      .post<AuthResponseData>(this.signUpApiUrl, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  signin(email: string, password: string): Observable<AuthResponseData> {
+    return this.http
+      .post<AuthResponseData>(this.signInApiUrl, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'Pojawił się nieznany błąd. Spróbuj ponownie.';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'Podany adres email już widnieje w bazie.';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Operacja niedozowolona.';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'Pojawił się nieznany błąd. Spróbuj później.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Podany adres email nie istnieje.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Niepoprawne hasło.';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'Użytkownik wyłączony przez administratora.';
+        break;
+      default:
+        errorMessage = 'Pojawił się nieznany błąd. Spróbuj ponownie.';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
