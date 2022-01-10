79f3a7ec86d6483fe498a00fd294fd98b945886a
Michał Skoczek <mwskoczek@gmail.com>
2022-01-10 15:12:38 +0100

    Added authService to register component and subscribe signup function

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,11 +8,23 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    console.log(form.valid);
+
+    if (!form.valid) return;
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.authService.signup(email, password).subscribe(
+      (res) => console.log(res),
+      (error) => console.log(error)
+    );
+
+    form.reset();
   }
 }
