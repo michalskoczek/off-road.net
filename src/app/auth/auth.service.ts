e65c500d753bc0e3b3cb119a211b8ed2b97a3814
Michał Skoczek <mwskoczek@gmail.com>
2022-01-14 18:08:54 +0100

    Add logout method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { User } from './user.model';
 import { webApiKey } from '../shared/private-properties';
+import { Router } from '@angular/router';
 
 interface AuthResponseData {
   idToken: string;
@@ -24,7 +25,7 @@ export class AuthService {
 
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string): Observable<AuthResponseData> {
     return this.http
@@ -64,6 +65,11 @@ export class AuthService {
           );
         })
       );
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/login']);
   }
 
   private handleAuthUser(
