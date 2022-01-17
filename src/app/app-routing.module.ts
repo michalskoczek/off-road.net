14b0fd368b6b2f8f75dace25579b6eb21a63ffae
Michał Skoczek <mwskoczek@gmail.com>
2022-01-17 14:27:22 +0100

    Split app module to small parts and app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth/auth.guard';
-import { AutoServicesComponent } from './auto-services/auto-services.component';
-import { BlogsComponent } from './blogs/blogs.component';
-import { CalendarEventFormComponent } from './calendar-event-form/calendar-event-form.component';
-import { CanDeactivateGuard } from './calendar-event-form/can-deactivate-guard.service';
-import { CalendarEventDetailComponent } from './calendar/calendar-event/calendar-event-detail/calendar-event-detail.component';
-import { CalendarEventComponent } from './calendar/calendar-event/calendar-event.component';
-import { CalendarListComponent } from './calendar/calendar-list/calendar-list.component';
-import { CalendarListResolver } from './calendar/calendar-list/calendar-list.resolver';
-import { CalendarComponent } from './calendar/calendar.component';
+
 import { HomepageComponent } from './homepage/homepage.component';
-import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { RegisterComponent } from './register/register.component';
-import { StoresComponent } from './stores/stores.component';
 
 const appRoutes: Routes = [
   {
@@ -22,73 +10,12 @@ const appRoutes: Routes = [
     component: HomepageComponent,
     pathMatch: 'full',
   },
-  {
-    path: 'calendar',
-    component: CalendarComponent,
-    // canActivate: [AuthGuard],
-    resolve: {
-      events: CalendarListResolver,
-    },
-    children: [
-      {
-        path: '',
-        component: CalendarListComponent,
-      },
-      {
-        path: 'event',
-        component: CalendarEventComponent,
-        children: [
-          {
-            path: ':id',
-            component: CalendarEventDetailComponent,
-            resolve: {
-              events: CalendarListResolver,
-            },
-          },
-          {
-            path: ':id/edit',
-            component: CalendarEventFormComponent,
-            canDeactivate: [CanDeactivateGuard],
-          },
-        ],
-      },
-      {
-        path: 'create-event',
-        component: CalendarEventFormComponent,
-      },
 
-      {
-        path: 'signup',
-        component: RegisterComponent,
-      },
-      {
-        path: '**',
-        redirectTo: '../**',
-      },
-    ],
-  },
-  {
-    path: 'services',
-    component: AutoServicesComponent,
-  },
-  {
-    path: 'stores',
-    component: StoresComponent,
-  },
-  {
-    path: 'blogs',
-    component: BlogsComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-
-  {
-    path: '**',
-    component: PageNotFoundComponent,
-    data: { errorMessage: 'Page not found!' },
-  },
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent,
+  //   data: { errorMessage: 'Page not found!' },
+  // },
 ];
 
 @NgModule({
