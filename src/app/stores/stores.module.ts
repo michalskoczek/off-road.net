b799807dcdfa34022652bb745ad2ad5a7770c09c
Michał Skoczek <mwskoczek@gmail.com>
2022-01-17 14:31:42 +0100

    Added stores module and stores-routing module

---
+++ src/app/stores/stores.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StoresRoutingModule } from './stores-routing.module';
+import { StoresComponent } from './stores.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [StoresComponent],
+  imports: [
+    CommonModule,
+    StoresRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+})
+export class StoresModule {}
