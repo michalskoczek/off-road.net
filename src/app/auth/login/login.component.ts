4111ddff783fba0d527872fb49f9e850491a844d
Michał Skoczek <mwskoczek@gmail.com>
2022-01-17 14:29:00 +0100

    Moved login and register dirs to auth dir

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  isLoading: boolean = false;
+  error: string = null;
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) return;
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+
+    this.authService.signin(email, password).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        this.router.navigate(['/calendar']);
+      },
+      error: (errorMessage) => {
+        this.error = errorMessage;
+        this.isLoading = false;
+      },
+    });
+
+    form.reset();
+  }
+}
