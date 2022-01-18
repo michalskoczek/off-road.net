3ce07ca76784cfc9307166053bfbff74c9ebfba4
Michał Skoczek <mwskoczek@gmail.com>
2022-01-18 17:35:38 +0100

    Removed unnecesary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import '@angular/common/locales/global/pl';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { CalendarModule } from './calendar/calendar.module';
-import { AutoServicesModule } from './auto-services/auto-services.module';
-import { StoresModule } from './stores/stores.module';
-import { BlogsModule } from './blogs/blogs.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, PageNotFoundComponent],
+  declarations: [AppComponent, HomepageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    AuthModule,
-    CalendarModule,
-    AutoServicesModule,
-    StoresModule,
-    BlogsModule,
-    SharedModule,
     CoreModule,
+    SharedModule,
+    AuthModule,
   ],
 
   bootstrap: [AppComponent],
