bb2362190a005f7a401c296a97e074a8c26fd60e
Michał Skoczek <mwskoczek@gmail.com>
2022-01-19 15:35:14 +0100

    Added api key to environment and environmet.prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  webApiKey: 'AIzaSyBOiZVG4Wl1mvp7qToawfnH7sO8xEKK4fs',
 };
 
 /*
