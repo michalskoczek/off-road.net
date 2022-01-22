baf864aa0cf67284161719afbea35ba2588dcc64
Michał Skoczek <mwskoczek@gmail.com>
2022-01-22 16:43:15 +0100

    Created interface options and adminOptions

---
+++ src/app/options-panel/options-panel.model.ts
@@ -0,0 +1,9 @@
+export interface Options {
+  optionsName: { typeOfEvent: string[]; placeOfEvent: string[] };
+  eventOptionsName: { favourite: string; participation: string[] };
+}
+
+export interface AdminOptions extends Options {
+  adminOptionsName: string[];
+  adminEventOptionsName: string[];
+}
